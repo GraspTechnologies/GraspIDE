3800e1427112d14f8857ecbc9ca3669d96c2f0d5
ChristianPanov <krissko7209@abv.bg>
2018-11-09 18:24:59 +0200

    Grasp 2019 2.0

---
+++ Grasp/Grasp/Controls/Editor/ErrorListControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Grasp
+{
+    public partial class ErrorListControl : UserControl
+    {
+        public ErrorListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
