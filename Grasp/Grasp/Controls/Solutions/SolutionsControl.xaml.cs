d326538ea84a762ddfbb39aa78b6c7a725794191
ChristianPanov <krissko7209@abv.bg>
2018-11-12 18:59:55 +0200

    Grasp

---
+++ Grasp/Grasp/Controls/Solutions/SolutionsControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Grasp
+{
+    public partial class SolutionsControl : UserControl
+    {
+        public SolutionsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
